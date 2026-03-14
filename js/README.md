82ecd526130aced7f535991a21254b6c07fce57a
Renan Giuliasse <renanduartebr.yahoo.com.br@gmail.com>
2026-03-14 03:33:56 -0300

    render lines pipeline

---
+++ js/README.md
@@ -0,0 +1,36 @@
+# Pontos 3D em 2D
+## Diferenças de espaço
+O espaço de um display é apenas 2D, ou seja, vive no espaço definido em $(x, y)$, enquanto o espaço 3D é regido pelo espaço $(x,y,z)$.
+
+![](screen-to-3d-question.png)
+
+Na conversão de espaços, precisamos normalizar ambos espaços definidos com seus pontos de origem $O$ definidos em $(0,0)$. Ambos espaços devem ser limitados lateralmente por $(-1,1)$ da direita para esquerda respectivamente e de baixo para cima respectivamente. Na imagem acima, estamos visualizando lateralmente a projeção sendo feita no plano $y'$.
+
+![](screen-projection.png)
+
+Precisamos aplicar uma transformação na visão da tela pra dar a sensação de profundidade do espaço 3D num plano 2D. Para isso, temos como regra que dois objetos, tanto na tela, quanto no espaço, **são proporcionais.**
+
+![](assets/latex-formula.png)
+
+Ajustando essa igualdade, temos que:
+
+![](assets/latex-formula(3).png)
+
+Entretanto, nessa aplicação eu preciso apenas transformar pontos 3D em pontos 2D, sem me preocupar com renderizar curvas de bezier ou coisas do tipo, então eu posso projetar o plano _display_ no plano _espacial_ sem levar em consideração matrizes de rotação e translação. Assim, simplifico esse projeto que demoraria semanas pra completar e eu nem tocaria nele durante anos...
+
+![](projecao%20simples.png)
+
+Com a imagem acima fica mais simples de entender, simplesmente convertemos o espaço para o plano ignorando o eixo (neste caso) $z$, e o modo que fazemos isso é simples:
+- Olhando para a tela de cima, você tem $x$ e $z$
+- Olhando de lado, $y$ e $z$
+Calculando a distancia focal de cima e dos lados, achamos a diferença entre o olho (da imagem) e o meio da tela $(0,0)$, criando um triangulo retângulo. Tendo a distancia do olho até a tela como $ds_{w}$
+
+![](assets/latex-formula(1).png)
+
+Tendo nossa distância do olho pra tela como $1$ (normalizando ambos os espaços), podemos simplificar para:
+
+![](assets/latex-formula(2).png)
+
+Sabendo que $x',x$ estão limitados por $-1,1$ e $y',y$ por $-1,1$. Agora podemos simplesmente transformar entre ambos espaços, projeção é simplificadamente basicamente só isso.
+
+![](https://hotsigns.net/assets/images/emogee%20247.png)
--- js/index.ts
+++ js/index.ts
@@ -9,7 +9,6 @@ const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
 // console.log(canvas, ctx);
 const [w, h] = [canvas.width, canvas.height];
-console.debug(w, h);
 
 function clear() {
     ctx.fillStyle = "gray";
@@ -30,6 +29,17 @@ function draw_point({ x, y }: Point) {
     const size = 10;
     ctx.fillStyle = "black";
     ctx.fillRect(x - size / 2, y - size / 2, size, size);
+}
+
+function draw_line({ x, y }: Point, { x: x1, y: y1 }: Point) {
+    const size = 2;
+
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = size;
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x1, y1);
+    ctx.stroke();
 }
 
 function to_screen({ x, y, z }: Point3D): Point {
@@ -77,21 +87,30 @@ function translate({ x, y }: Point, { x: dx = 0, y: dy = 0 }: Point): Point {
 
 let dt = 0;
 
+const vertecies: Point3D[] = [
+    { x: 0.5, y: 0.5, z: -0.5 }, // 0
+    { x: -0.5, y: 0.5, z: -0.5 }, // 1
+    { x: -0.5, y: 0.5, z: 0.5 }, // 2
+    { x: 0.5, y: 0.5, z: 0.5 }, // 3
+
+    { x: 0.5, y: -0.5, z: -0.5 }, // 4
+    { x: 0.5, y: -0.5, z: 0.5 }, // 5
+    { x: -0.5, y: -0.5, z: 0.5 }, // 6
+    { x: -0.5, y: -0.5, z: -0.5 }, // 7
+];
+
+const faces = [
+    [0, 1, 2, 3],
+    [4, 5, 6, 7],
+    [0, 4],
+    [5, 3],
+    [2, 6],
+    [1, 7],
+];
+
 function render_vertecies() {
-    const fps = 240;
+    const fps = 180;
     dt = 1 / fps;
-
-    let vertecies: Point3D[] = [
-        { x: 0.5, y: 0.5, z: -0.5 },
-        { x: 0.5, y: -0.5, z: -0.5 },
-        { x: -0.5, y: 0.5, z: -0.5 },
-        { x: -0.5, y: -0.5, z: -0.5 },
-
-        { x: 0.5, y: 0.5, z: 0.5 },
-        { x: 0.5, y: -0.5, z: 0.5 },
-        { x: -0.5, y: 0.5, z: 0.5 },
-        { x: -0.5, y: -0.5, z: 0.5 },
-    ];
 
     let ds = 0;
     setInterval(() => {
@@ -101,25 +120,54 @@ function render_vertecies() {
             if (!vertecies[i]) {
                 continue;
             }
-            // vertecies[i] = translate3d(vertecies[i]!, {
-            //     x: 0,
-            //     y: 0,
-            //     z: 10 * dt,
-            // });
-            // vertecies[i] = rotate_z(vertecies[i]!, 1 * dt);
-            draw_point(
-                vect_to_screen(
-                    to_screen(
-                        translate3d(rotate_z(vertecies[i]!, ds), {
-                            x: 0,
-                            y: 0,
-                            z: 500 * dt,
-                        }),
-                    ),
-                ),
-            );
+            let point_0 = translate3d(rotate_z(vertecies[i]!, ds), {
+                x: 0,
+                y: 0,
+                z: 2,
+            });
+            draw_point(vect_to_screen(to_screen(point_0)));
         }
     }, 1000 / fps);
 }
 
-render_vertecies();
+function render_lines() {
+    const fps = 240;
+    dt = 1 / fps;
+
+    let ds = 0;
+    setInterval(() => {
+        clear();
+        ds += 0.8 * dt;
+        for (const face of faces) {
+            // 0 1 2 3
+            // ^-^-^-^ -> 0
+            for (let i = 0; i < face.length; i++) {
+                const vertice = face[i]!;
+                const next_vertice = face[(i + 1) % face.length]!;
+
+                let point_0 = translate3d(rotate_z(vertecies[vertice]!, ds), {
+                    x: 0,
+                    y: 0,
+                    z: 2,
+                });
+                let point_1 = translate3d(
+                    rotate_z(vertecies[next_vertice]!, ds),
+                    {
+                        x: 0,
+                        y: 0,
+                        z: 2,
+                    },
+                );
+                draw_line(
+                    vect_to_screen(to_screen(point_0)),
+                    vect_to_screen(to_screen(point_1)),
+                );
+            }
+        }
+        // draw_point(vect_to_screen(to_screen(point_0)));
+    }, 1000 / fps);
+}
+
+// !!! ONE RENDER AT A TIME, RACE CONDITION ADVISED.
+// render_vertecies();
+render_lines();
